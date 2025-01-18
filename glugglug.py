3e3595d5fcaaf1939507b5ee78e5c107f2b0cb6d
Brandon <101031607+itsbrandonaw@users.noreply.github.com>
2025-01-18 18:04:59 +0800

    Update glugglug.py

--- glugglug.py
+++ glugglug.py
@@ -1,3 +1,4 @@
+from typing import Final
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes, ConversationHandler
 from datetime import datetime, timedelta
